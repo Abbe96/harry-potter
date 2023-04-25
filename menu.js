94740caf1c7d9b0aadd22fb61978fb80f412e4d6
Elvira Ericsson <elvira.rigi@hotmail.com>
2023-04-25 21:18:47 +0200

    update

---
+++ menu.js
@@ -0,0 +1,31 @@
+document.querySelector(".changeHousesMenu").addEventListener("click", changeHouses);
+document.querySelector(".charactersMenu").addEventListener("click", goToCharacters);
+document.querySelector(".housesMenu").addEventListener("click", goToHouses);
+document.querySelector(".filmsMenu").addEventListener("click", goToFilms);
+document.querySelector(".resultMenu").addEventListener("click", goToInfo);
+console.log("Hej");
+
+
+function goToHouses () {
+    document.querySelector("#menu").style.display = "none";
+    document.querySelector("").style.display = "block";
+}
+
+function goToFilms () {
+    document.querySelector("#menu").style.display = "none";
+    document.querySelector("").style.display = "block";
+}
+
+function goToCharacters () {
+    document.querySelector("#menu").style.display = "none";
+    document.querySelector("").style.display = "block";
+}
+
+function goToInfo () {
+    document.querySelector("#menu").style.display = "none";
+    document.querySelector("").style.display = "block";
+}
+
+function changeHouses () {
+
+}
