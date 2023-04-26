f100c8f5cf8b12df10c5a1203d2cdd05e7114a95
Elvira Ericsson <elvira.rigi@hotmail.com>
2023-04-26 15:06:41 +0200

    update

--- menu.js
+++ menu.js
@@ -1,9 +1,12 @@
-document.querySelector(".changeHousesMenu").addEventListener("click", changeHouses);
 document.querySelector(".charactersMenu").addEventListener("click", goToCharacters);
 document.querySelector(".housesMenu").addEventListener("click", goToHouses);
 document.querySelector(".filmsMenu").addEventListener("click", goToFilms);
-document.querySelector(".resultMenu").addEventListener("click", goToInfo);
-console.log("Hej");
+document.querySelector(".infoMenu").addEventListener("click", goToInfo);
+document.querySelector(".changeHousesGryffindor").addEventListener("click", changeHousesGryffindor);
+document.querySelector(".changeHousesSlytherin").addEventListener("click", changeHousesSlytherin);
+document.querySelector(".changeHousesRavenclaw").addEventListener("click", changeHousesRavenclaw);
+document.querySelector(".changeHousesHufflepuff").addEventListener("click", changeHousesHufflepuff);
+console.log("Hello");
 
 
 function goToHouses () {
@@ -26,6 +29,16 @@ function goToInfo () {
     document.querySelector("").style.display = "block";
 }
 
-function changeHouses () {
+function changeHousesGryffindor () {
+    document.querySelector("#menu").style.backgroundColor = "#a61d1d";
+}
 
+function changeHousesSlytherin () {
+    document.querySelector("#menu").style.backgroundColor = "#37671c";
+}
+function changeHousesRavenclaw () {
+    document.querySelector("#menu").style.backgroundColor = "#1c4b67";
+}
+function changeHousesHufflepuff () {
+    document.querySelector("#menu").style.backgroundColor = "#d6ce24";
 }
