b81d5cf09a5799fdd58ea9c001655ceaec37999c
Pernilla Nadie <pernilla.nadie@outlook.com>
2023-05-01 22:12:28 +0200

    added characters.php

---
+++ api/characters.php
@@ -0,0 +1,34 @@
+<?php
+    echo "Hello";
+    ini_set("display_erros", 1);
+
+    $json = file_get_contents("characters.json");
+
+    if ($json === false) {
+        echo "Error, failed to read json-file";
+        exit;
+    }
+
+    $characters = json_decode($json, true);
+
+    if ($characters === null) {
+        echo "Error, failed to decode json";
+        exit;
+    }
+
+    foreach ($characters as $character) {
+        if (!isset($character["name"]) || !isset($character["link"])) {
+            echo "Error, missin keys";
+            exit;
+        }
+
+        $response[] = [
+            "name" => $character["name"],
+            "link" => $character["link"]
+        ];
+    }
+
+    header("Content-Type: application/json");
+    echo json_encode($response);
+
+?>
