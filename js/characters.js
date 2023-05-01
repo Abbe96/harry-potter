fefc8faf52a0a030d091bdf7a33518cc84864644
Pernilla Nadie <pernilla.nadie@outlook.com>
2023-05-01 22:11:09 +0200

    characters.js

---
+++ js/characters.js
@@ -0,0 +1,34 @@
+document.querySelector("body").style.backgroundImage = "url(Images-characters/background.png)";
+
+async function loadCharacters() {
+    try {
+        const response = await fetch("characters.json");
+        const data = await response.json();
+  
+        const charactersDiv = document.getElementById("characters");
+  
+        Object.keys(data).forEach(character => {
+            //create newcharacterDiv element
+            const characterDiv = document.createElement("div");
+            characterDiv.classList.add("name", character);
+            characterDiv.style.backgroundImage = `url(${data[character].imageSource})`;
+    
+            //add eventlisteners to characterDiv element
+            characterDiv.addEventListener('mouseover', function() {
+                characterDiv.innerHTML = `${data[character].name}<br>${data[character].house}`;
+                //characterDiv.style.filter = "blur(3px)";
+            });
+    
+            characterDiv.addEventListener('mouseleave', function() {
+                characterDiv.innerHTML = "";
+                //characterDiv.style.filter = "none";
+            });
+            //append characterDiv element to charactersDiv container
+            charactersDiv.appendChild(characterDiv);
+        });
+    } catch (error) {
+      console.error(error);
+    }
+}
+  
+loadCharacters();
