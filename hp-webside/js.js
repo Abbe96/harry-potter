5e920d0bc3bad3ebc0167b2aea6752be25547ac4
Abbe96 <113461041+Abbe96@users.noreply.github.com>
2023-05-02 10:37:21 +0200

    9

--- hp-webside/js.js
+++ hp-webside/js.js
@@ -8,7 +8,12 @@ const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI =
 "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 const info = [];
-
+async function getMovies(url) {
+    const resp = await fetch(url);
+    const respData = await resp.json();
+    console.log(respData);
+    console.log(respData.results[0])
+}
 const main = document.getElementById("content");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
---
+++ hp-webside/test.js
@@ -0,0 +1,2 @@
+var element = document.getElementById("idh1");
+  element.innerHTML = countries[1].Code+" "+ countries[1].Continent;
