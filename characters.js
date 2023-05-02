8d432519e4c436f5be0ad497b12fb4ca028b3d0a
Elvira Ericsson <elvira.rigi@hotmail.com>
2023-05-02 10:24:33 +0200

    updates

--- characters.js
+++ characters.js
@@ -1,4 +1,5 @@
 document.querySelector("body").style.backgroundImage = "url(Images-characters/background.png)";
+document.querySelector(".menuBar").style.backgroundImage = "url(Images-characters/lightning.png)";
 
 async function loadCharacters() {
     try {
@@ -6,22 +7,26 @@ async function loadCharacters() {
         const data = await response.json();
   
         const charactersDiv = document.getElementById("characters");
-  
+        //create info tag and add it to the characterDiv
+        const info = document.createElement("a");
+
         Object.keys(data).forEach(character => {
-            //create newcharacterDiv element
+            //create characterDiv element, and class .name
             const characterDiv = document.createElement("div");
             characterDiv.classList.add("name", character);
             characterDiv.style.backgroundImage = `url(${data[character].imageSource})`;
     
             //add eventlisteners to characterDiv element
             characterDiv.addEventListener('mouseover', function() {
-                characterDiv.innerHTML = `${data[character].name}<br>${data[character].house}`;
                 //characterDiv.style.filter = "blur(3px)";
+                info.href = data[character].info;               
+                info.innerHTML = `${data[character].name}<br>${data[character].house}`;             
+                characterDiv.appendChild(info); 
             });
     
             characterDiv.addEventListener('mouseleave', function() {
-                characterDiv.innerHTML = "";
                 //characterDiv.style.filter = "none";
+                characterDiv.innerHTML = "";
             });
             //append characterDiv element to charactersDiv container
             charactersDiv.appendChild(characterDiv);
