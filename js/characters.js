93f3e3ac7d5d6dba50ae28da06c676009870c8f0
Pernilla Nadie <pernilla.nadie@outlook.com>
2023-05-02 11:01:46 +0200

    will edid more

--- js/characters.js
+++ js/characters.js
@@ -9,6 +9,10 @@ async function loadCharacters() {
     </div>
     `;
 
+    const charactersDiv = main.getElementById("characters");
+    //create info tag and add it to the characterDiv
+    const info = main.createElement("a")
+
     try {
         const response = await fetch("api/characters.json");
         const data = await response.json();
@@ -38,4 +42,46 @@ async function loadCharacters() {
       console.error(error);
     }
 }
+
+/*
+document.querySelector("body").style.backgroundImage = "url(Images-characters/background.png)";
+document.querySelector(".menuBar").style.backgroundImage = "url(Images-characters/lightning.png)";
+
+async function loadCharacters() {
+    try {
+        const response = await fetch("characters.json");
+        const data = await response.json();
+  
+        const charactersDiv = document.getElementById("characters");
+        //create info tag and add it to the characterDiv
+        const info = document.createElement("a");
+
+        Object.keys(data).forEach(character => {
+            //create characterDiv element, and class .name
+            const characterDiv = document.createElement("div");
+            characterDiv.classList.add("name", character);
+            characterDiv.style.backgroundImage = `url(${data[character].imageSource})`;
+    
+            //add eventlisteners to characterDiv element
+            characterDiv.addEventListener('mouseover', function() {
+                //characterDiv.style.filter = "blur(3px)";
+                info.href = data[character].info;               
+                info.innerHTML = `${data[character].name}<br>${data[character].house}`;             
+                characterDiv.appendChild(info); 
+            });
+    
+            characterDiv.addEventListener('mouseleave', function() {
+                //characterDiv.style.filter = "none";
+                characterDiv.innerHTML = "";
+            });
+            //append characterDiv element to charactersDiv container
+            charactersDiv.appendChild(characterDiv);
+        });
+    } catch (error) {
+      console.error(error);
+    }
+}
+  
+loadCharacters();
+*/
   
